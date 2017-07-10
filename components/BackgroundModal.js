3b476cc03c43da7603f148add1f1c55829dfa494
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-07-10 18:25:03 +0700

    Add BackgroundModal to directory components

---
+++ components/BackgroundModal.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+const BackgroundModal = styled.View`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    backgroundColor: gray;
+    opacity: 0.8;
+`;
+
+export default BackgroundModal;
