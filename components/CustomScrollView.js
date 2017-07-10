0aef61c7be9ecf754c1f7aa6f979415ac1b032a6
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-07-10 12:19:23 +0700

    Add CumtomScrollView component in project

---
+++ components/CustomScrollView.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+const CustomScrollView = styled.ScrollView`
+    flex: 1;
+    backgroundColor: #fff;
+    flexDirection: column;
+`;
+
+export default CustomScrollView;
