b5039677d80b5fbec8bc63c318b3b732c6abeb50
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-07-10 18:27:37 +0700

    Edit detail UI block profile

--- screens/ProfileScreen/BlockProfile.js
+++ screens/ProfileScreen/BlockProfile.js
@@ -1,16 +1,80 @@
 import React from 'react';
 import {
     View,
-    Text,
-    StyleSheet
+    StyleSheet,
 } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+//components
+import CustomScrollView from '../../components/CustomScrollView';
+import TextTitie from '../../components/TextTitle';
+import CustomButton from '../../components/CustomButton';
+import ModalEditIcon from './ModalEditIcon';
+
+//style
+import styles from './styles/BlockProfileStyle';
 
 
 function BlockProfile(props) {
-    return (
-        <View>
+    let { profile, icons, colors } = props;
 
-        </View>
+    return (
+        <CustomScrollView>
+            <View style={styles.blockRootProfile}>
+                {/*Title*/}
+                <TextTitie>
+                    Your Profile 
+                </TextTitie>
+                {/*End Title Icon*/}
+
+                {/*Icon*/}
+                <Entypo
+                    name={profile.icon} size={160} 
+                    color={profile.color} 
+                />
+                {/*End Icon*/}
+
+                {/*Name*/}
+                <View style={styles.blockName}>
+                    <TextTitie> 
+                        Name: 
+                    </TextTitie> 
+
+                    <TextTitie 
+                        textColor="#337ab7" 
+                    > 
+                        {" " + profile.name} 
+                    </TextTitie> 
+                </View> 
+                {/*End Name*/}
+
+                {/*Edit Icon and Color*/} 
+                <View style={styles.blockEditButton}> 
+                    <CustomButton 
+                        styleContainer={styles.btnEditIcon} 
+                        textColor="white" 
+                    > 
+                        Edit Icon 
+                    </CustomButton> 
+                    <CustomButton 
+                        styleContainer={styles.btnEditColor} 
+                        textColor="black" 
+                    >
+                        Edit Color
+                    </CustomButton>
+                    <CustomButton
+                        styleContainer={styles.btnEditIcon}
+                        textColor="white"
+                    >
+                        Edit Name
+                    </CustomButton>
+                </View>
+                {/*Edit Icon and Color*/}
+
+                <ModalEditIcon>
+
+                </ModalEditIcon>
+            </View>
+        </CustomScrollView>
     );
 }
 
