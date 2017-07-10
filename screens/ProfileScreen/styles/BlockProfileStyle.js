2794a813f5120931d51b3e5cdfdaac6f8be12fcc
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-07-10 18:27:12 +0700

    Add stylesheet to BlockProfile

---
+++ screens/ProfileScreen/styles/BlockProfileStyle.js
@@ -0,0 +1,44 @@
+import { 
+    StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    blockRootProfile: {
+        flex: 1,
+        flexDirection: 'column',
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        // backgroundColor: 'blue',
+        margin: 10
+    },
+
+    blockName: {
+        marginVertical: 5,
+        flexDirection: 'row'
+    },
+
+    blockEditButton: {
+        marginVertical: 30,
+        flex: 1,
+        alignSelf: 'stretch',
+        // backgroundColor: 'blue',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+
+    btnEditIcon: {
+        backgroundColor: '#337ab7',
+        marginVertical: 5,
+        width: 250,
+        borderRadius: 50
+    },
+
+    btnEditColor: {
+        backgroundColor: '#dddddd',
+        marginVertical: 5,
+        width: 250,
+        borderRadius: 50
+    },
+});
+
+export default styles;
