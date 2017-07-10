233f2fa66ab6714a5b34694125352839863a4821
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-07-10 18:25:34 +0700

    Add CustomButton to directory components

---
+++ components/CustomButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native';
+//component
+import TextTitle from './TextTitle';
+
+/**
+ * 
+ * @param {*} props is object  
+ * @param props.styleContainer is root style of component (stylesheet)
+ * @param props.children is title of component 
+ * @param props.handler is callback function call when click button 
+ * @param props.textColor color of text
+ */
+function CustomButton(props) {
+    let {
+        styleContainer,
+        children,
+        handler,
+        textColor
+    } = props;
+
+    //TODO onpress
+    return (
+        <TouchableOpacity style={[ styles.blockButton, styleContainer ]}>
+            <TextTitle textColor={textColor} size={17}>
+                { children }
+            </TextTitle>
+        </TouchableOpacity>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    blockButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 30,
+        backgroundColor: 'red'
+    }
+});
+
+export default CustomButton;
